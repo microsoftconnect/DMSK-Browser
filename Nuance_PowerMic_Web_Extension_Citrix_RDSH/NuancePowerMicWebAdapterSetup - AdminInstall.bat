09f94385cb6a1da1f8f9fb693f1e7b69e5c6ffd3
John Desmond <johndesmond@microsoft.com>
2025-03-17 07:25:33 -0400

    Added PowerMic web extension docs and install package

---
+++ Nuance_PowerMic_Web_Extension_Citrix_RDSH/NuancePowerMicWebAdapterSetup - AdminInstall.bat
@@ -0,0 +1,20 @@
+@ECHO off
+
+>nul 2>&1 "%SYSTEMROOT%\system32\cacls.exe" "%SYSTEMROOT%\system32\config\system"
+@IF %ERRORLEVEL% NEQ 0 GOTO ELEVATE
+@GOTO ADMINTASKS
+
+:ELEVATE
+
+mshta "javascript: var shell = new ActiveXObject('Shell.Application'); shell.ShellExecute('%~nx0', '', '', 'runas', 1); close();"
+EXIT /B
+
+:ADMINTASKS
+
+: Elevated commands
+
+cd /d %~dp0
+:installing NuancePowerMicWebAdapterSetup...
+start NuancePowerMicWebAdapterSetup_123.1.25.0.exe /CentralDeployment
+
+EXIT
